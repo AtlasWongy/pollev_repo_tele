fcaff0e4ba88a0376355c03332a3d63aa089f1b4
MamoMamo99 <aaronsng96@gmail.com>
2022-10-12 23:59:34 +0800

    Updates to check for polls changing the question
    - Remove the timer
    - Add text file to cross check with changing questions

--- aaron_class_automate/attendance_automation.py
+++ aaron_class_automate/attendance_automation.py
@@ -18,29 +18,30 @@ username_nus = aaron_instance.username_nus
 password = aaron_instance.password
 
 # Ask Aaron which presentation is it tomorrow
-while True:
-    aaron_lecture_tomorrow = input("Hello, which lecture is it tomorrow?: ")
+# while True:
+#     aaron_lecture_tomorrow = input("Hello, which lecture is it tomorrow?: ")
 
-    if aaron_lecture_tomorrow == "mmecon":
-        presentation_room = config_settings['presentation_mmecon']
-        presentation_time = "17:51:00"
-        break
-    elif aaron_lecture_tomorrow == "denistkachen573":
-        presentation_room = config_settings['presentation_denistkachen573']
-        presentation_time = "09:00:00"
-        break
-    else:
-        continue
+#     if aaron_lecture_tomorrow == "mmecon":
+#         presentation_room = config_settings['presentation_mmecon']
+#         presentation_time = "12:00:00"
+#         break
+presentation_room = config_settings['presentation_denistkachen573']
+#     elif aaron_lecture_tomorrow == "joji":
+#         presentation_room = config_settings['presentation_denistkachen573']
+#         presentation_time = "14:10:00"
+#         break
+#     else:
+#         continue
 
 # Check for the time to run the script
-now = datetime.now()
-current_time = str(now.strftime("%H:%M:%S"))
-print("Current Time = ", current_time)
+# now = datetime.now()
+# current_time = str(now.strftime("%H:%M:%S"))
+# print("Current Time = ", current_time)
 
-while current_time != presentation_time:
-    now = datetime.now()
-    current_time = str(now.strftime("%H:%M:%S"))
-    print(f"Current time is now: {current_time}, presentation not yet started")
+# while current_time != presentation_time:
+#     now = datetime.now()
+#     current_time = str(now.strftime("%H:%M:%S"))
+#     print(f"Current time is now: {current_time}, presentation not yet started")
 
 print("Script will now run....")
 
--- aaron_class_automate/enter_presentation_room.py
+++ aaron_class_automate/enter_presentation_room.py
@@ -3,9 +3,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from telegram_bot_send.send_reminder import send_the_reminder
+import linecache
 
 def presentation_enter(driver, room):
 
@@ -36,16 +38,30 @@ def presentation_enter(driver, room):
     while loop_again:
 
         try:
+            time.sleep(5)
             WebDriverWait(driver, some_timeout).until(
                 EC.element_to_be_clickable((By.CLASS_NAME, waiting_presentation_start_banner_class))
             )
             driver.refresh()
-        except NoSuchElementException:
+        except TimeoutException:
+
+            check_last_poll_questions = linecache.getlines("constants.txt", -1)
+            
+            questions_class_name = "component-response-header__title"
+            get_the_question_web_element = WebDriverWait(driver, some_timeout).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, questions_class_name))
+            )
+
+            get_the_question_text = get_the_question_web_element.text
+
+            if check_last_poll_questions[-1].strip() == get_the_question_text:
+                driver.refresh()
+                continue
 
             # Send telegram notification
             send_the_reminder()
             loop_again = False
-        
+   
     # Let aaron fill in his answers
 
     doing_the_poll_loop = True
@@ -55,7 +71,20 @@ def presentation_enter(driver, room):
         checks_input = input("Waiting for aaron input: ")
 
         if checks_input == "aarondone":
-            print("End Script")
+            print("Grab the last question")
+
+            # Save the poll into a text file
+            question_title_class_name = "component-response-header__title"
+            save_the_last_question_web_element = WebDriverWait(driver, some_timeout).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, question_title_class_name))
+            )
+            save_the_last_question_text = save_the_last_question_web_element.text
+
+            with open("constants.txt", "w") as text_file:
+                text_file.write(save_the_last_question_text)
+            
+            print("End the script")
+
             return 
         else:
             continue
